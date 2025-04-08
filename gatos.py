345463ccedb186eb93e3d145c4e68131156b38c5
ericramirezz <eric.ram.val@gmail.com>
2025-04-08 15:42:14 -0600

    primer commit interno

---
+++ gatos.py
@@ -0,0 +1,46 @@
+import threading
+from time import sleep
+from random import randint
+
+class gatito(threading.Thread): ##Herencia de threading.Thread Para la independencia de cada gato
+
+    ##Constructor de la clase gatito
+    def __init__(self, nombre, tipo, plato):
+        threading.Thread.__init__(self) ##llama a la clase para que garantixce el ser un hilo
+        self.nombre=nombre ##Por que cada gato tiene un nombre
+        self.tipo=tipo     ##Si es hembra, macho o invasor
+        self.estado = "hambriento" ##Define el estado que tendra el gato
+        self.porciones = 0  ##cuantas porciones ha comido el gato
+        self.plato = plato  ##en que plato esta llevando la accion, para el considerarlo en el semaphoro
+    """INTRODUCIR ATRIBUTOS PARA QUE LA CLAS EPUEDA INTERACTUAR CON LA INTERFA<"""        
+    ##funcion para que el hilo trabaje
+    def run(self):
+        ##cilo para que se repita esta acción
+        while True:
+            self.comer() ##llamado a comer
+            sleep(randint(1,19))     ##Tiempo de reposo, al ser un gato, podriamos decir que duerme o hace cualquier otra cosa de gatos
+    
+    ##funcion para que el gato coma
+    def comer(self):
+        ##asignando las porciones que come cada tipo de gato
+        if self.tipo.upper() == "MACHO" :
+            porciones = 2
+        elif self.tipo.upper() == "HEMBRA":
+            porciones = 1 
+        else:
+            porciones = randint(1,3) ##puesto que no se sabe cuando va a comer
+
+
+        #accesos al plato, donde se verificara que no este lleno y asi 
+        if self.plato.semaforo.acquire(blocking=True): #SI el metodo dice que si hay suficiente comida en el plato comera
+            self.estado="comiendo" 
+            sleep(randint(1,3))     #tiempo que se tarda en comer
+            self.estado="satisfecho"
+        else:
+            self.estado="hambriento"
+        
+        self.plato.semaforo.realease() #libera el semaforo 
+        
+  """INTRODUCIR METODOS PARA LA INTERFAZ GRAFICA"""        
+"""INTRODUCIR ACTUALIZACION EN LOS METODOS PARA LA INTERFAZ GRAFICA """    
+
---
+++ platos.py
@@ -0,0 +1,41 @@
+##Librerias a importar
+import threading
+from time import sleep
+from random import randint
+
+class Plato:
+    def __init__(self,num):
+        self.num=num ##numero de plato
+        self.comida_disponible = 5 ##porsiones en el plato 
+        self.semaforo = threading.Semaphore(2) ## semaforo para dos gato simultaneos
+        self.lock = threading.Lock() ## lock para poder bloquear la concurrencia en el plato 
+    
+    def comiendo(self, porcion):
+        with self.lock ##bloqueanod para que no puedan mas de dos gatos comer al mismo tiempo
+            if self.porciones >= porcion:   ##validadno que haya suficiente comida
+                self.comida_disponible -= porcion ##reajustando las cantidades
+                print(f"El plato {self.num} ahora tiene {self.comida_disponible} porciones")    ##mensaje
+                return True             ##indicacion de que si comio
+            else:
+                print(f"El plato {self.num} no tiene suficientes porciones")
+                return False            #3indicacion de que no comió
+
+    def rellenar(self):
+        with self.lock ##lockeo para que no se lastime ningun gato(si no se hace se mezclan las interacciones)
+            if self.comida_disponible < 5:
+                self.comida_disponible += 3
+                if self.comida_disponible > 5:
+                    self.comida_disponible = 5
+                    print(f"Ohhh no, se ha desvordado la comida, ahora tendras que limpiar")
+                print(f"El plato {self.num} ha sido re abastecido")
+        
+
+    def estado(self):
+        with self.lock:
+            print(f"Planto {self.num} con {self.comida_disponible} porciiones")
+    
+
+
+  """INTRODUCIR METODOS PARA LA INTERFAZ GRAFICA"""        
+"""INTRODUCIR ACTUALIZACION EN LOS METODOS PARA LA INTERFAZ GRAFICA """    
+
