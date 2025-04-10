504f040ac6700c34a8677be0df15615db84ae46e
ericramirezz <eric.ram.val@gmail.com>
2025-04-10 12:51:52 -0600

    cola, y v1 main

--- Dispensador.py
+++ Dispensador.py
@@ -2,6 +2,8 @@ import threading
 from time import sleep
 from random import randint
 from enum import Enum
+from Gato import Gato
+from Plato import Plato
 
 class EstadoDispensador(Enum):
     CERRADO = 1
@@ -10,11 +12,13 @@ class EstadoDispensador(Enum):
 
 class Dispensador:
     
-    def __init__(self, platos): # Inicializa el dispensador con los platos que administra a traves de una lista llamada platos
-        self.platos= platos
-        self.estado = Dispensador.CERRADO
-        self.gato_invasor = False
-    
+    def __init__(self, platos, gatos, cola): # Inicializa el dispensador con los platos que administra a traves de una lista llamada platos
+        self.platos = platos # Lista de platos
+        self.estado = Dispensador.CERRADO # Estado inicial del dispensador
+        self.gato_invasor = False # Indica si hay un gato invasor presente
+        self.gatos = gatos # Lista de gatos que pueden acceder al dispensador
+        self.cola = cola # Cola de gatos que esperan para acceder a los platos
+         
     def dispensar(self, cantidad):
         # Simula el tiempo de dispensado
         sleep(1)
@@ -27,8 +31,11 @@ class Dispensador:
         self.estado = EstadoDispensador.ALERTA
     
     def retiradaGatoInvasor(self):
-      with self.Lock:
         self.gato_invasor = False
         self.estado = EstadoDispensador.ABIERTO
         self.dispensar()
         self.estado = EstadoDispensador.CERRADO
+        
+    def insertarGatoCola(self, gato):
+        self.cola.put(gato.nivelPrioridad(), gato.nombre)
+        
